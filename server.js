fcc188a07ce34692529ddeae715824b82cc376e0
mindtouchben <benr@mindtouch.com>
2018-01-23 12:53:42 -0800

    Updated the CORS header on server.js

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'https://benr-demo.mindtouch.us');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE');
 
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
