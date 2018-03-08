e32670545e342da3903e0139de7d080c72396d8c
mindtouchben <benr@mindtouch.com>
2018-03-08 09:43:15 -0800

    Added the ability to store pagetitle

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -74,7 +74,7 @@ router.post('/', (req, res) => {
                     res.status(400).send('Record already exsists');
                 } else {
                     query = `INSERT INTO notifications (userid, email, username, pageid, pageurl, pagetitle) 
-                        VALUES (${userid}, '${useremail}', '${username}', ${pageid}, '${pageurl}', '${pagetitle});`
+                        VALUES (${userid}, '${useremail}', '${username}', ${pageid}, '${pageurl}', '${pagetitle}');`
                         connection.query(query, function (error, results, fields) {
                             if (results.affectedRows > 0) {
                                 res.send('Added new record');
