8d288a3a3815afe2c8667a5a66dcc3334baeb495
mindtouchben <benr@mindtouch.com>
2018-03-13 13:10:09 -0700

    fixed failure

--- server.js
+++ server.js
@@ -24,7 +24,6 @@ connection.connect(function(err) {
     console.log('connected as id ' + connection.threadId);
 });
 
-app.use(express.static('static'));
 app.use(bodyParser.json({limit: '50mb'}));
 
 app.use(cors());
