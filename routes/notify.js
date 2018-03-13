8df0791e156e95542b09b50dd6168587e39e468e
mindtouchben <benr@mindtouch.com>
2018-03-13 13:25:15 -0700

    bug fix

--- routes/notify.js
+++ routes/notify.js
@@ -29,6 +29,7 @@ router.post('/', (req, res) => {
     if (pageid != undefined && pageurl != undefined && pageTitle != undefined) {
         // get all users by email subscribed to the page
         var query = `SELECT * FROM notifications WHERE pageid ='${pageid}' AND pageurl = "?"`;
+        console.log(query);
         connection.query(query, [pageTitle], function (error, results, fields) {
             if (error) {
                 console.log(error);
